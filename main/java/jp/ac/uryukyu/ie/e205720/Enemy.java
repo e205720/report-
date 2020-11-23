d057e70724659344b25bffd702597bf73a0a1657
Naito TAKERU <e205720@ie.u-ryukyu.ac.jp>
2020-11-23 13:26:39 +0900

    3st commit

--- main/java/jp/ac/uryukyu/ie/e205720/Enemy.java
+++ main/java/jp/ac/uryukyu/ie/e205720/Enemy.java
@@ -1,26 +1,23 @@
 package jp.ac.uryukyu.ie.e205720;
 
 /**
- * 敵クラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
+ * 敵クラス。 String name; //敵の名前 int hitPoint; //敵のHP int attack; //敵の攻撃力 boolean
+ * dead; //敵の生死状態。true=死亡。 Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
+     * 
+     * @param name      モンスター名
      * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
+     * @param attack    モンスターの攻撃力
      */
-    public Enemy (String name, int maximumHP, int attack) {
+    public Enemy(String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
         this.attack = attack;
@@ -29,14 +26,14 @@ public class Enemy {
     }
 
     /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * Heroへ攻撃するメソッド。 attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * 
      * @param hero 攻撃対象
      */
-    public void attack(Hero hero){
-        if  (hitPoint > 0){
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+    public void attack(Hero hero) {
+        if (hitPoint > 0) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
         }
         
@@ -54,5 +51,19 @@ public class Enemy {
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
     }
+    public boolean isDead() {
+        return dead;
+    }
 
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+    
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+    }
 }
